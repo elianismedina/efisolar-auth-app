db57396e04ce632571e8c8df7955954e78244d7b
elianismedina <elianismedina05@outlook.com>
2025-01-28 10:45:43 -0500

    added props to header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,17 +1,23 @@
+"use client";
 import React from "react";
 import MainNav from "./main-nav";
 import MobileNav from "./mobile-nav";
+import { useEffect } from "react";
+import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
 
-export default function Header() {
+export default function Header({ pageProps }: AppProps) {
+  const router = useRouter();
+  useEffect(() => {}, [router]);
   return (
     <header className="sticky top-0 w-full border-b bg-amber-400">
       <div className="h-14 container">
         {/* Desktop */}
-        <MainNav />
+        <MainNav {...pageProps} />
 
         {/* Mobile */}
 
-        <MobileNav />
+        <MobileNav {...pageProps} />
       </div>
     </header>
   );
