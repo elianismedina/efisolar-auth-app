10044e98a8576795845e7fc0d432dd41d30e9698
elianismedina <elianismedina05@outlook.com>
2025-01-28 21:06:52 -0500

    changed gradient

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,7 +4,7 @@ import MobileNav from "./mobile-nav";
 
 export default function Header() {
   return (
-    <header className="sticky top-0 w-full border-b bg-gradient-to-r from-zinc-700 to-zinc-800">
+    <header className="sticky top-0 w-full border-b bg-gradient-to-r from-neutral-300 to-stone-400">
       <div className="h-16 flex items-center justify-end px-4 md:px-8">
         {/* Desktop */}
         <MainNav />
