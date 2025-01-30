98c431f3f6808fae5b0eb574c636e010cdb2e689
elianismedina <elianismedina05@outlook.com>
2025-01-30 16:07:46 -0500

    added whatsapp floating icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 import Header from "../components/header";
 import { TailwindIndicator } from "../components/tailwind-indicator";
 import Footer from "../components/footer";
+import WhatsappLive from "../components/whatsapp-live";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -28,6 +29,7 @@ export default function RootLayout({
           <Header />
           {children}
           <Footer />
+          <WhatsappLive />
           <TailwindIndicator />
         </SessionProvider>
       </body>
