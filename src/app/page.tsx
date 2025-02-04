fcb495c7149fd017931235cb55a4c65528ea54fd
elianismedina <elianismedina05@outlook.com>
2025-02-04 18:49:15 -0500

    changed style in hero text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,18 +3,20 @@ import { Button } from "../components/ui/button";
 
 export default function Home() {
   return (
-    <main className="flex flex-col md:flex-row md:gap-16 items-center justify-center gap-6 px-3 py-2 mx-auto max-w-7xl">
+    <main className="flex flex-col md:flex-row md:gap-16 items-center justify-center gap-6 px-3 mx-auto max-w-7xl">
       <div className="p-8">
         <h1 className="text-center md:text-left text-4xl font-bold">
           Potencia tu negocio con <br />{" "}
           <span className="text-primary">Agentes de IA personalizados</span>
         </h1>
         <p className=" text-center md:text-left mt-4">
-          Somos tu aliado ideal para implementar soluciones de IA personalizadas
+          Somos tu aliado ideal para implementar{" "}
+          <span className="font-semibold">soluciones de IA</span> personalizadas
           y escalarlas sin empleados adicionales. <br /> No requiere
-          conocimientos técnicos - Nosotros nos enacargamos de todo. <br />{" "}
-          Lidera el cambio, aumenta la productividad y deja que la
-          automatización impulse tu crecimiento.
+          conocimientos técnicos-Nosotros nos enacargamos de todo. <br /> Lidera
+          el cambio, aumenta la productividad y deja que la{" "}
+          <span className="font-semibold">automatización</span>
+          impulse tu crecimiento.
         </p>
         <div className="flex flex-row gap-4">
           <Button className="mt-6">Comenzar ahora</Button>
