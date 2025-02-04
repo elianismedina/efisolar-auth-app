1fcc6da24b055eab11a2eb0997627423ab40cb28
elianismedina <elianismedina05@outlook.com>
2025-02-04 11:03:10 -0500

    changhed image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import { Button } from "../components/ui/button";
 
-export default async function Home() {
+export default function Home() {
   return (
     <main className="flex flex-col md:flex-row md:gap-16 items-center justify-center gap-6 px-3 py-10 mx-auto max-w-7xl">
       <div className="p-8">
